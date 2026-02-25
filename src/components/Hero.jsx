067b0c7442b90924fd366567af1061f1f44b8afe
arzoosingh147 <arzoosingh147@gmail.com>
2026-02-25 21:11:06 +0530

    minor change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import meme from "../assets/hero-meme.png"; // your chosen meme
 
 const Hero = () => {
   return (
-    <section id="home" className="h-screen flex items-center px-6 md:px-12 lg:px-24 py-10 mt-20 bg-[#6c6964] ">
+    <section id="home" className="h-screen flex items-center px-6 md:px-12 lg:px-24 py-10 mt-20 bg-[#E4C896] ">
       <div className="w-full md:w-1/2">
         <h1 className="text-5xl font-bold mb-6"> Hiiii, I'm <span className="text-7xl text-white text-shadow-lg/30 " > Arzoo </span></h1>
         <p className="mb-4 text-gray-600">I am a Frontend Developer| I like to build things without planning and let my brain go with the flow(Works 95% of the time | Source: TRUST ME BRO!!!!!!! )</p>
